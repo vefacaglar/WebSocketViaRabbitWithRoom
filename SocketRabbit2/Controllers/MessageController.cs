1e60aa021fb411877d47c94ba7db1755dbe391c0
Vefa Caglar <vefacaglarozcan@gmail.com>
2024-07-03 23:27:21 +0300

    added send message endpoint

---
+++ SocketRabbit2/Controllers/MessageController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SocketRabbit2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+        private readonly RabbitMqService _queueService;
+
+        public MessageController(
+            RabbitMqService queueService
+            )
+        {
+            _queueService = queueService;
+        }
+
+        [HttpGet]
+        public IActionResult Send(string room, string message)
+        {
+            _queueService.PublishMessage(room, message);
+            return Ok();
+        }
+    }
+}
